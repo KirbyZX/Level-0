c655ef67abb194acf2293b5b61c47bb5b5317054
Kirbster <lucas.kirby@gmail.com>
2017-02-26 17:31:20 +0000

    Add files via upload
    Moving platform and enemy class files

---
+++ platform_moving.py
@@ -0,0 +1,59 @@
+import pygame
+
+from classes.platform import Platform
+
+
+class MovingPlatform(Platform):
+    """ Platform that can move """
+
+    change_x = 0
+    change_y = 0
+
+    boundary_top = 0
+    boundary_bottom = 0
+    boundary_left = 0
+    boundary_right = 0
+
+    player = None
+    level = None
+
+    def update(self):
+        """ Move the platform. Will shove the player out of the way. """
+
+        # Move left/right
+        self.rect.x += self.change_x
+
+        # See if we hit the player
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            # Hit the player. Move the player around.
+
+            # If we are moving right, set our right side
+            # to the left side of the item we hit
+            if self.change_x < 0:
+                self.player.rect.right = self.rect.left
+            else:
+                # Otherwise if we are moving left, do the opposite.
+                self.player.rect.left = self.rect.right
+
+        # Move up/down
+        self.rect.y += self.change_y
+
+        # Check and see if we the player
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            # Hit the player. Move the player.
+
+            # Reset our position based on the top/bottom of the object.
+            if self.change_y < 0:
+                self.player.rect.bottom = self.rect.top
+            else:
+                self.player.rect.top = self.rect.bottom
+
+        # Check the boundaries and see if we need to reverse direction.
+        if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+            self.change_y *= -1
+
+        cur_pos = self.rect.x - self.level.level_shift
+        if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+            self.change_x *= -1
