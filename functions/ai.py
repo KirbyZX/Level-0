1b522d3316775b52c990d9157846e8923f4c81a7
KirbyZX <lucas.kirby@gmail.com>
2017-02-28 19:48:37 +0000

    Upgraded the AI function. now jumps when needed.

--- functions/ai.py
+++ functions/ai.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 def ai(enemy, player):
     """ Basic A.I. for enemies """
 
@@ -9,4 +12,16 @@ def ai(enemy, player):
         enemy.stop()
 
     if enemy.rect.y < player.rect.y:
+        enemy.jump()
+
+    enemy.rect.x += 2
+    platform_hit_list = pygame.sprite.spritecollide(enemy, enemy.level.platform_list, False)
+    enemy.rect.x -= 2
+    if len(platform_hit_list) > 0:
+        enemy.jump()
+
+    enemy.rect.x -= 2
+    platform_hit_list = pygame.sprite.spritecollide(enemy, enemy.level.platform_list, False)
+    enemy.rect.x += 2
+    if len(platform_hit_list) > 0:
         enemy.jump()
