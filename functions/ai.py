46d99b3af875c11bb27c351c6f30d530ef8ab234
YifBookchainer <YifBookchainer@gmail.com>
2017-03-04 15:16:15 +0000

    Update ai.py

--- functions/ai.py
+++ functions/ai.py
@@ -1,9 +1,11 @@
 import pygame
 import math as maths
                     #attacks              speed hp  graphic     
-enemies={"rifleman":[[["bullet", 6.3, 1]], 2.1, 24, "rifleman", ]}
+enemies={"rifleman":[[["bullet", 6.3, 1]], 2.1, 24, "rifleman"]}
                      #atk type,speed,cooldown
 
+g=2.1 #Lucas, use a variable for gravity, then remove this.
+  
 #Hey Lucas, k is for knowledge - you can change that if you want to.
 #Also, please make a bullets variable (a list of all bullet positions and velocities).
 #enemies will also need an attacks list. So that we can have different types.
@@ -15,12 +17,12 @@ def ai(enemy, player, k):
     """ Basic A.I. for enemies """
     ep=[enemy.rect.x, enemy.rect.y]
     pp=[player.rect.x, player.rect.y]
-    op=200
+    op=k[enemy.type()] 
     att=enemies[enemy.type()]#attributes
     
-    if (ep[0]**2-pp[0]**2)+(ep[1]**2-pp[1]**2)>op**2:
+    if (ep[0]**2-pp[0]**2)+(ep[1]**2-pp[1]**2)>op[0]**2:
         enemy.go_left()
-    elif (ep[0]**2-pp[0]**2)+(ep[1]**2-pp[1]**2)<dp**2:
+    elif (ep[0]**2-pp[0]**2)+(ep[1]**2-pp[1]**2)<dp[0]**2:
         enemy.go_right()
     else:
         enemy.stop()
@@ -30,12 +32,12 @@ def ai(enemy, player, k):
             ua=maths.atan((midpos[1]-pop[1])//(midpos[0]-pop[0]))
             addpx =atk[1]*maths.cos(us)
             addpy=atk[1]*maths.sin(ua)
-            bullets+=[[enemy.rect.x, enemy.rect.y, addpx, addpy]]
+            bullets+=[[enemy.rect.x, enemy.rect.y, addpx, addpy, epos]]
 
 
-    enemy.rect.x+=att[1]
+    enemy.rect.y+=g
     platform_hit_list = pygame.sprite.spritecollide(enemy, enemy.level.platform_list, False)
-    enemy.rect.x -= att[1]
+    enemy.rect.y -= g
     if len(platform_hit_list) > 0:
         enemy.jump()
 
