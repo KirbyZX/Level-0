68b1ece97f662621f4f66d1968da67ec97910c75
KirbyZX <lucas.kirby@gmail.com>
2017-03-07 19:03:35 +0000

    Rifleman class has speed, hp and can shoot bullets.

--- classes/rifleman.py
+++ classes/rifleman.py
@@ -1,4 +1,7 @@
+import pygame
+
 from classes.enemy import Enemy
+from classes.bullet import Bullet
 
 
 class Rifleman(Enemy):
@@ -13,10 +16,22 @@ class Rifleman(Enemy):
         # Parent constructor
         Enemy.__init__(self, player)
 
-        self.hp = 0
-        self.attacks = []
+        self.hp = 24
+        self.speed = 5
 
+        self.bullet_list = pygame.sprite.Group()
 
+    def shoot(self):
+        """ Shooting a bullet """
 
+        bullet = Bullet(self)
+        bullet.rect.x = self.rect.x
+        bullet.rect.y = self.rect.y
 
+        self.bullet_list.add(bullet)
 
+    def go_left(self):
+        super().left(self.speed)
+
+    def go_right(self):
+        super().right(self.speed)
