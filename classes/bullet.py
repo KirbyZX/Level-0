b10ec48fdbe5e3cf7994d4565f01b9f4ce0b8b2d
YifBookchainer <YifBookchainer@gmail.com>
2017-03-11 12:50:35 +0000

    Update bullet.py

--- classes/bullet.py
+++ classes/bullet.py
@@ -19,6 +19,19 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self):
         """ Move the bullet. """
-        self.rect.x += self.add_x
-        self.rect.y += self.add_y
+        pp=[player.rect.x, player.rect.y]
+        bullet.rect.x+=bullet.add_x
+        bullet.rect.y+=bullet.add_y
+        bullet.rect.y+=gravity_acceleration
+        if pp[0]<b[0]<pp[0]+20 and pp[1]+30<b[1]<pp[1]+84:  # SHOT!
+            player.hp -= 36
+            k[bullet.shotby] += [bullet.shotat, bullet.shotat]
+            bullet.remove(bullet)
+        if pp[0]+20<b[0]<pp[0]+50 and pp[1]+30<b[1]<pp[1]+84:  # SHOT!
+            player.hp -= 12
+            k[bullet.shotby] += [bullet.shotat]
+            bullet.remove(bullet)
+        elif [b[0] - b[0] % 50, b[1] - b[1] % 50] in platform_list:
+            bullets.remove(bullet)
+            if bullet.shotat in k[bullet.shotby]: k[b[5]].remove(b[4])
         # Need to add direction
