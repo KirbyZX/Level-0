47ecb6c623a40d259ecbab2f31b1abb730c66190
KirbyZX <lucas.kirby@gmail.com>
2017-03-14 18:24:01 +0000

    Cleaned up (deleted) some stuff. Jack, you need to put the code about the player getting shot in the main loop. Use each enemy's bullet_list.

--- classes/bullet.py
+++ classes/bullet.py
@@ -1,6 +1,6 @@
 import pygame
 
-from classes.constants import *
+from constants import *
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -8,30 +8,21 @@ class Bullet(pygame.sprite.Sprite):
     Represents a bullet.
     """
 
-    def __init__(self):
+    def __init__(self, shooter):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
         self.image = pygame.Surface([10, 3])
         self.image.fill(BLACK)
 
+        self.change_x = 15
+
+        self.shooter = shooter
+
         self.rect = self.image.get_rect()
 
     def update(self):
         """ Move the bullet. """
-        pp=[player.rect.x, player.rect.y]
-        bullet.rect.x+=bullet.add_x
-        bullet.rect.y+=bullet.add_y
-        bullet.rect.y+=gravity_acceleration
-        if pp[0]<b[0]<pp[0]+20 and pp[1]+30<b[1]<pp[1]+84:  # SHOT!
-            player.hp -= 36
-            k[bullet.shotby] += [bullet.shotat, bullet.shotat]
-            bullet.remove(bullet)
-        if pp[0]+20<b[0]<pp[0]+50 and pp[1]+30<b[1]<pp[1]+84:  # SHOT!
-            player.hp -= 12
-            k[bullet.shotby] += [bullet.shotat]
-            bullet.remove(bullet)
-        elif [b[0] - b[0] % 50, b[1] - b[1] % 50] in platform_list:
-            bullets.remove(bullet)
-            if bullet.shotat in k[bullet.shotby]: k[b[5]].remove(b[4])
-        # Need to add direction
+
+        self.rect.x += self.change_x
+        # Jack, you put the getting shot part in the main loop
